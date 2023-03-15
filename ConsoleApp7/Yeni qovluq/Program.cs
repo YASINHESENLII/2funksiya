fa1d01b8ad81db20fa49f03faf386694eaa8d187
YASINHESENLII <yasinhesenli44@gmail.com>
2023-03-15 04:19:33 +0400

    2

---
+++ ConsoleApp7/Yeni qovluq/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] ints = { 1, 2, 5, 4, 5 };
+        int value = 5;
+        Func(ints, value);
+    }
+
+    public static int Func(int[] ints, int value)
+    {
+        int number = 0;
+        for (int i = 0; i < ints.Length; i++)
+        {
+            if (ints[i] == value)
+            {
+                number++;
+            }
+        }
+        Console.WriteLine(number);
+        return number;
+    }
+}
